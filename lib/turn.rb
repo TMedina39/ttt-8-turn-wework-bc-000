3672e0243ef72815c569f826cf56a73646871185
Tomas Medina <tomas.medina@wework.com>
2018-02-17 18:47:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,5 +41,6 @@ def move(board, index, character = "X")
 end
 #===================================
 def turn(board)
-  puts "Please enter 1-9:"
+  #puts "Please enter 1-9:"
+  puts"Please enter 1-9:"
 end
