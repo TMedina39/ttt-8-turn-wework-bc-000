821fe6f139721f15e8064c4672b5064016743e88
Tomas Medina <tomas.medina@wework.com>
2018-02-17 18:48:09 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,6 +41,6 @@ def move(board, index, character = "X")
 end
 #===================================
 def turn(board)
-  #puts "Please enter 1-9:"
-  puts"Please enter 1-9:"
+  puts "Please enter 1-9:"
+    input = gets.strip
 end
